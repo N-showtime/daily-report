e8838f8e305ffdb578a69a2e95bef6fa8cb77f89
Shogo.N <hozukl.1225@gmail.com>
2025-09-14 22:47:38 +0900

    フォームの内容を送信・保存

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc } from "firebase/firestore";
 
 // 設定情報
 const firebaseConfig = {
@@ -37,4 +37,26 @@ if ( document.getElementById("js-history")) {
   fetchHistoryData();
 }
 
+//Cloud Firestoreにデータを送信する
+const submitData = async (e) => {
+  e.preventDefault();
 
+  const formData =new FormData(e.target);
+
+  try {
+    const docRef = await addDoc(collection(db, "reports"), {
+      date: new Date(),
+      name: formData.get("name"),
+      work: formData.get("work"),
+      comment: formData.get("comment")
+  });
+    console.log("Document written with ID:", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+}
+
+//Cloud Firestoreにデータを送信する
+if (document.getElementById("js-form")) {
+  document.getElementById("js-form").addEventListener("submit", (e) => submitData(e));
+};
